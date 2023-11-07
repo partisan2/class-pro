2ef31da575ac88aa079d115d95ef829faf40b7da
Vihan Gayathra <s92068813@ousl.lk>
2023-11-07 08:33:56 +0530

    updated

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import LogOut from './LogOut'
 import { Link } from 'react-router-dom'
+// import Header from './Header'
 
 function Dashboard() {
   return (
     <div>
+      {/* <Header/> */}
       DashBoard
       <LogOut/>
       UpdateProfile<Link to='/update-profile'>Update</Link>
