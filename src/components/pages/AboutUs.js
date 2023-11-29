cd2c7d1af698659155370c61a851a1b6214d757d
ShaliTharuka <140537325+ShaliTharuka@users.noreply.github.com>
2023-11-29 19:37:33 +0530

    About us

---
+++ src/components/pages/AboutUs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeaderDashboard from '../HeaderDashboard'
+import FooterDashboard from '../FooterDashboard'
+
+function AboutUs() {
+  return (
+    <div>
+        <HeaderDashboard/>
+        <div>
+            
+        </div>
+        <FooterDashboard/>
+
+
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeaderDashboard from '../HeaderDashboard'
+import FooterDashboard from '../FooterDashboard'
+
+function Contact() {
+  return (
+    <div>
+        <HeaderDashboard/>
+
+        <FooterDashboard/>
+    </div>
+  )
+}
+
+export default Contact
