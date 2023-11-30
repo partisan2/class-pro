aa22bbeb1e8de24076e58e4fabdd55901d3cb655
Vihan Gayathra <s92068813@ousl.lk>
2023-11-30 14:59:57 +0530

    profile changed

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import HeaderDashboard from '../HeaderDashboard'
 import FooterDashboard from '../FooterDashboard'
 import LogOut from './LogOut'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../contexts/AuthContext'
+import { db } from '../../firebase'
+import { doc,getDoc } from "firebase/firestore";
+
+
+
 
 function Profile() {
+  const [ userName,setUserName ] = useState()
+  const [ userEmail,setUserEmail ] = useState()
+  const [ userType,setUserType ] = useState()
+  const [ userId,setUserID ] = useState()
+  const {currentUser} = useAuth();
+
+
+  useEffect(()=>{
+    const fetchData = async () =>{
+      try{
+        const docRef = doc(db, "Users", currentUser.uid);
+        const docSnap = await getDoc(docRef);
+        
+        if (docSnap.exists()) {
+          // console.log("Document data:", docSnap.data().email);
+          setUserName(docSnap.data().userName)
+          setUserEmail(docSnap.data().email)
+          setUserType(docSnap.data().userType)
+          setUserID(docSnap.data().userId)
+          // console.log(docSnap.data().profilePic)
+        } else {
+          // docSnap.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      }catch(r){console.log(r)}
+    }
+    fetchData()
+  },[currentUser.uid])
   return (
     <div>
       <HeaderDashboard/>
@@ -13,18 +47,24 @@ function Profile() {
         <br/>
         <tr>
           <td className='name'><label>User Name</label></td>
-          <td><label>vihan</label></td>
+          <td>{userName}</td>
         </tr>
         <br></br>
         <tr>
           <td className='name'><label>User Email</label></td>
-          <td><label>test@mail</label></td>
+          <td>{userEmail}</td>
         </tr>
         <br/>
         <tr>
           <td className='name'><label>User Type</label></td>
-          <td><lable>Teacher</lable></td>
+          <td>{userType}</td>
         </tr>
+        <br/>
+        <tr>
+          <td className='name'><label>User ID</label></td>
+          <td>{userId}</td>
+        </tr>
+        <br></br>
         <span>
           <Link to='/update-profile'>Update profile</Link>
         </span>
@@ -32,6 +72,7 @@ function Profile() {
         <span>
           <Link to='/update-password'>update password</Link>
         </span>
+        <br/>
         <span>
           <LogOut/>
           <br/>
