a174b47c705705743fe1e9cc0c130212dcf527c4
ShaliTharuka <140537325+ShaliTharuka@users.noreply.github.com>
2023-12-01 10:01:15 +0530

    Update Dashboard

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -33,6 +33,62 @@ function Dashboard() {
     fetchData()
   },[])
 
+<<<<<<< Updated upstream
+=======
+  // get assignments
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      let list =[]
+      try{
+        const date = new Date()
+        const day = date.getDate()
+        const month = date.getMonth()+1
+        const year = date.getFullYear()
+        const currentDate = `${year}-${month}-${day}`
+        console.log(currentDate)
+        
+        const querySnapshot = await getDocs(query(collection(db, "Assignments"),orderBy("assignmentDue")));
+        querySnapshot.forEach((doc) => {
+        list.push({ id : doc.id, assignmentName: doc.data().assignmentName,assignmentDue: doc.data().assignmentDue})
+      });
+      setAssignments(list)
+      // console.log(list)
+
+      }catch(r){console.log(r)}
+    }
+    fetchData()
+  },[])
+
+  //get all students
+  useEffect(()=>{
+    const fetchData = async () =>{
+      let list = []
+      try{
+        const querySnapshot = await getDocs(query(collection(db, "Users"),where("userType","==","student")));
+        querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data());
+        list.push({ id : doc.id ,...doc.data()})
+      });
+      setUsers(list)
+    //   console.log(list)
+
+      }catch(r){console.log(r)}
+    }
+    fetchData()
+  },[])
+
+  //---------object destruction
+  const studentList = users?.map(({userName,email,userId},index)=>{
+    return <StudentListLayout
+    key={index}
+    userName={userName}
+    userId={userId}
+    email={email}
+    />
+  })
+
+>>>>>>> Stashed changes
   const upcomingEvent = events?.map(({eventName,eventDate,eventTime},index)=>{
     return <UpcomingEvent
     key={index}
