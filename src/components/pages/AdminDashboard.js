726af98c47ef65504e70b402c8fdd4315757312e
Vihan Gayathra <s92068813@ousl.lk>
2023-12-02 15:21:25 +0530

    admin dashboard updated

--- src/components/pages/AdminDashboard.js
+++ src/components/pages/AdminDashboard.js
@@ -1,19 +1,110 @@
-import React from 'react'
-import TeacherListLayout from './TeacherListLayout'
+import React,{useEffect, useState} from 'react'
+import { db } from '../../firebase'
+import { getDocs,collection,query,where } from "firebase/firestore";
+import UserListLayout from './UserListLayout';
+import './admin.css'
 
 function AdminDashboard() {
+    const [ teacherList,setTeacherList ] = useState()
+    const [ parentList,setParentList ] =useState()
+    const [ studentList,setStudentList ] = useState()
+    
+    //get all teacher
+  useEffect(()=>{
+    const fetchData = async () =>{
+      let list = []
+      try{
+        const querySnapshot = await getDocs(query(collection(db, "Users"),where("userType","==","teacher")));
+        querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data());
+        list.push({ id : doc.id ,...doc.data()})
+      });
+      setTeacherList(list)
+    //   console.log(list)
+
+      }catch(r){console.log(r)}
+    }
+    fetchData()
+  },[])
+
+  const teachers = teacherList?.map(({userName,email,userType},index)=>{
+    return <UserListLayout
+    key={index}
+    userName={userName}
+    email={email}
+    userType={userType}
+    />
+  })
+
+  //get parents
+  useEffect(()=>{
+    const fetchData = async () =>{
+      let list = []
+      try{
+        const querySnapshot = await getDocs(query(collection(db, "Users"),where("userType","==","parent")));
+        querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data());
+        list.push({ id : doc.id ,...doc.data()})
+      });
+      setParentList(list)
+    //   console.log(list)
+
+      }catch(r){console.log(r)}
+    }
+    fetchData()
+  },[])
+
+  const parents = parentList?.map(({userName,email,userType},index)=>{
+    return <UserListLayout
+    key={index}
+    userName={userName}
+    email={email}
+    userType={userType}
+    />
+  })
+
+  //get students
+  useEffect(()=>{
+    const fetchData = async () =>{
+      let list = []
+      try{
+        const querySnapshot = await getDocs(query(collection(db, "Users"),where("userType","==","student")));
+        querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data());
+        list.push({ id : doc.id ,...doc.data()})
+      });
+      setStudentList(list)
+      console.log(list)
+
+      }catch(r){console.log(r)}
+    }
+    fetchData()
+  },[])
+
+  const students = studentList?.map(({userName,email,userType},index)=>{
+    return <UserListLayout
+    key={index}
+    userName={userName}
+    email={email}
+    userType={userType}
+    />
+  })
+ 
   return (
     <div className='admin-dashboard'>
         <section className='container'>
           <div className='user-list'>
             <div className='teacher-list'>
-              <TeacherListLayout/>
+              {teachers}
             </div>
             <div className='parent-list'>
-              parents
+              {parents}
             </div>
             <div className='students-list'>
-              students
+              {students}
             </div>
             <div className='not-assigned-users'>
               not assigned
