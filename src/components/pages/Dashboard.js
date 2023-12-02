39127f2673fcd9dbb99ffbce79b63aeaa564fef6
rinas21 <s92064902@ousl.lk>
2023-12-02 16:12:41 +0530

    footer updated

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,172 +1,224 @@
-import React, { useEffect, useState } from 'react'
-import HeaderDashboard from '../HeaderDashboard'
-import FooterDashboard from '../FooterDashboard'
-import { db } from '../../firebase'
-import { getDocs,collection,query,orderBy, where,doc,getDoc } from "firebase/firestore";
-import { useAuth } from '../../contexts/AuthContext'
-import "../Style.css"
-import { Link } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import HeaderDashboard from "../HeaderDashboard";
+import FooterDashboard from "../FooterDashboard";
+import { db } from "../../firebase";
+import {
+  getDocs,
+  collection,
+  query,
+  orderBy,
+  where,
+  doc,
+  getDoc,
+} from "firebase/firestore";
+import { useAuth } from "../../contexts/AuthContext";
+import "../Style.css";
+import { Link } from "react-router-dom";
+import DesignerImage from "./img/Designer.jpeg";
 
 function Dashboard() {
-  const [ events,setEvents ] = useState([])
-  const [ assignments,setAssignments ] = useState([])
-  const {currentUser} = useAuth();
-  const [ userType, setUserType ] =useState()
-  const [ users,setUsers ] = useState()
+  const [events, setEvents] = useState([]);
+  const [assignments, setAssignments] = useState([]);
+  const { currentUser } = useAuth();
+  const [userType, setUserType] = useState();
+  const [users, setUsers] = useState();
   //---------fetch Data
   // get usertype
-  useEffect(()=>{
-    const fetchData = async () =>{
-      try{
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
         const docRef = doc(db, "Users", currentUser.uid);
         const docSnap = await getDoc(docRef);
-        
+
         if (docSnap.exists()) {
           // console.log("Document data:", docSnap.data().email);
-          setUserType(docSnap.data().userType)
+          setUserType(docSnap.data().userType);
           // console.log(docSnap.data().profilePic)
         } else {
           // docSnap.data() will be undefined in this case
           console.log("No such document!");
         }
-      }catch(r){console.log(r)}
-    }
-    fetchData()
-  },[currentUser.uid])
+      } catch (r) {
+        console.log(r);
+      }
+    };
+    fetchData();
+  }, [currentUser.uid]);
   // get ucoming events
-  useEffect(()=>{
-    const fetchData = async ()=>{
-      let list =[]
-      try{
-        const date = new Date()
-        const day = date.getDate()
-        const month = date.getMonth()+1
-        const year = date.getFullYear()
-        const currentDate = `${year}-${month}-${day}`
+  useEffect(() => {
+    const fetchData = async () => {
+      let list = [];
+      try {
+        const date = new Date();
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        const currentDate = `${year}-${month}-${day}`;
         // console.log(date)
-        
-        const querySnapshot = await getDocs(query(collection(db, "Events"),where("eventDate",">=",currentDate),orderBy("eventDate")));
-        querySnapshot.forEach((doc) => {
-        list.push({ id : doc.id, eventName: doc.data().eventName,eventDate: doc.data().eventDate , eventTime: doc.data().eventTime})
-      });
-      setEvents(list)
-      // console.log(list)
 
-      }catch(r){console.log(r)}
-    }
-    fetchData()
-  },[])
+        const querySnapshot = await getDocs(
+          query(
+            collection(db, "Events"),
+            where("eventDate", ">=", currentDate),
+            orderBy("eventDate")
+          )
+        );
+        querySnapshot.forEach((doc) => {
+          list.push({
+            id: doc.id,
+            eventName: doc.data().eventName,
+            eventDate: doc.data().eventDate,
+            eventTime: doc.data().eventTime,
+          });
+        });
+        setEvents(list);
+        // console.log(list)
+      } catch (r) {
+        console.log(r);
+      }
+    };
+    fetchData();
+  }, []);
 
   // get assignments
-  useEffect(()=>{
-    const fetchData = async ()=>{
-      let list =[]
-      try{
-        const date = new Date()
-        const day = date.getDate()
-        const month = date.getMonth()+1
-        const year = date.getFullYear()
-        const currentDate = `${year}-${month}-${day}`
-        console.log(currentDate)
-        
-        const querySnapshot = await getDocs(query(collection(db, "Assignments"),orderBy("assignmentDue")));
-        querySnapshot.forEach((doc) => {
-        list.push({ id : doc.id, assignmentName: doc.data().assignmentName,assignmentDue: doc.data().assignmentDue})
-      });
-      setAssignments(list)
-      // console.log(list)
+  useEffect(() => {
+    const fetchData = async () => {
+      let list = [];
+      try {
+        const date = new Date();
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        const currentDate = `${year}-${month}-${day}`;
+        console.log(currentDate);
 
-      }catch(r){console.log(r)}
-    }
-    fetchData()
-  },[])
+        const querySnapshot = await getDocs(
+          query(collection(db, "Assignments"), orderBy("assignmentDue"))
+        );
+        querySnapshot.forEach((doc) => {
+          list.push({
+            id: doc.id,
+            assignmentName: doc.data().assignmentName,
+            assignmentDue: doc.data().assignmentDue,
+          });
+        });
+        setAssignments(list);
+        // console.log(list)
+      } catch (r) {
+        console.log(r);
+      }
+    };
+    fetchData();
+  }, []);
 
   //get all students
-  useEffect(()=>{
-    const fetchData = async () =>{
-      let list = []
-      try{
-        const querySnapshot = await getDocs(query(collection(db, "Users"),where("userType","==","student")));
+  useEffect(() => {
+    const fetchData = async () => {
+      let list = [];
+      try {
+        const querySnapshot = await getDocs(
+          query(collection(db, "Users"), where("userType", "==", "student"))
+        );
         querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, " => ", doc.data());
-        list.push({ id : doc.id ,...doc.data()})
-      });
-      setUsers(list)
-    //   console.log(list)
-
-      }catch(r){console.log(r)}
-    }
-    fetchData()
-  },[])
+          // doc.data() is never undefined for query doc snapshots
+          // console.log(doc.id, " => ", doc.data());
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setUsers(list);
+        //   console.log(list)
+      } catch (r) {
+        console.log(r);
+      }
+    };
+    fetchData();
+  }, []);
 
   //---------object destruction
-  const studentList = users?.map(({userName,email,userId},index)=>{
-    return <StudentListLayout
-    key={index}
-    userName={userName}
-    userId={userId}
-    email={email}
-    />
-  })
+  const studentList = users?.map(({ userName, email, userId }, index) => {
+    return (
+      <StudentListLayout
+        key={index}
+        userName={userName}
+        userId={userId}
+        email={email}
+      />
+    );
+  });
 
-  const upcomingEvent = events?.map(({eventName,eventDate,eventTime},index)=>{
-    return <UpcomingEvent
-    key={index}
-    eventName={eventName}
-    eventDate={eventDate}
-    eventTime={eventTime}
-    />
-  })
+  const upcomingEvent = events?.map(
+    ({ eventName, eventDate, eventTime }, index) => {
+      return (
+        <UpcomingEvent
+          key={index}
+          eventName={eventName}
+          eventDate={eventDate}
+          eventTime={eventTime}
+        />
+      );
+    }
+  );
 
-  const upcomingAssignments = assignments?.map(({assignmentName,assignmentDue},index)=>{
-    return <UpcomingAssignment
-    key={index}
-    assignmentName={assignmentName}
-    assignmentDue={assignmentDue}
-    />
-  })
+  const upcomingAssignments = assignments?.map(
+    ({ assignmentName, assignmentDue }, index) => {
+      return (
+        <UpcomingAssignment
+          key={index}
+          assignmentName={assignmentName}
+          assignmentDue={assignmentDue}
+        />
+      );
+    }
+  );
 
   return (
     <div>
-      <HeaderDashboard/>
-      <div className='dashboard-container'>
-        <div className='banner'>
-          banner
+      <HeaderDashboard />
+      <div className="dashboard-container">
+        <div className="banner">
+          <img
+            src={DesignerImage}
+            alt="Designer"
+            height="500px"
+            width="500px"
+          />
         </div>
         <div>
-          <div className='upcming-act'>
-            <span className='upcming-name'>Upcoming Assignments</span>
+          <div className="upcming-act">
+            <span className="upcming-name">Upcoming Assignments</span>
             {upcomingAssignments}
-            <span><Link to='/assignments'>Assignments</Link></span>
+            <span>
+              <Link to="/assignments">Assignments</Link>
+            </span>
           </div>
-          <br/>
+          <br />
           {/* upcoming event */}
-          <div className='upcming-event'>
-            <span className='upcming-name'>Upcoming Events</span>
+          <div className="upcming-event">
+            <span className="upcming-name">Upcoming Events</span>
             {upcomingEvent}
-            <span><Link to='/events'>Events</Link></span>
+            <span>
+              <Link to="/events">Events</Link>
+            </span>
           </div>
         </div>
       </div>
-      {userType === "teacher" && 
-      <div className='student-list'>
-        <h3>student list</h3>
-        {studentList}
-      </div>}
-      <FooterDashboard/>
+      {userType === "teacher" && (
+        <div className="student-list">
+          <h3>student list</h3>
+          {studentList}
+        </div>
+      )}
+      <FooterDashboard />
     </div>
-  )
+  );
 }
 
-
 //-------------- Layouts----------------------------------
-function UpcomingEvent({eventName,eventDate,eventTime}){
-  return(
+function UpcomingEvent({ eventName, eventDate, eventTime }) {
+  return (
     <table>
       <tr>
         <th>{eventName}</th>
+        <th>Time</th>
       </tr>
       <tr>
         <td>{eventDate}</td>
@@ -176,36 +228,41 @@ function UpcomingEvent({eventName,eventDate,eventTime}){
   );
 }
 
-function UpcomingAssignment({assignmentName,assignmentDue}){
-  return(
-    <table>
-      <tbody>
-      <tr>
-        <th>{assignmentName}</th>
-      </tr>
-      <tr>
-        <td>{assignmentDue}</td>
-      </tr>
-      </tbody>
-    </table>
-  )
-}
-
-function StudentListLayout({userName,email,userId}){
-  return(
-    <div>
+function UpcomingAssignment({ assignmentName, assignmentDue }) {
+  return (
     <table>
       <tbody>
         <tr>
-          <td><p>{userId}</p></td>
-          <td><p>{email}</p></td>
-          <td><p>{userName}</p></td>
+          <th>{assignmentName}</th>
+        </tr>
+        <tr>
+          <td>{assignmentDue}</td>
         </tr>
       </tbody>
     </table>
-    </div>
-  )
+  );
 }
 
+function StudentListLayout({ userName, email, userId }) {
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <p>{userId}</p>
+            </td>
+            <td>
+              <p>{email}</p>
+            </td>
+            <td>
+              <p>{userName}</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
-export default Dashboard
+export default Dashboard;
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,83 +1,92 @@
-import React, { useRef, useState } from 'react'
-import { useAuth } from '../../contexts/AuthContext'
-import { Link, useNavigate } from 'react-router-dom'
-import './SignUp.css'
-import Header from '../Header'
+import React, { useRef, useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import "./SignUp.css";
+import Header from "../Header";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
-import { db } from '../../firebase';
+import { db } from "../../firebase";
 
 function SignUp() {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const confirmPasswordRef = useRef()
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
 
-    const userIdRef = useRef()
+  const userIdRef = useRef();
 
-    const { signup } = useAuth()
-    const [error,setError] = useState()
-    const [loading,setLoading] = useState(false)
-    const navigate = useNavigate();
+  const { signup } = useAuth();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    async function handleSubmit(e){
-        e.preventDefault()
-        if(passwordRef.current.value !== confirmPasswordRef.current.value){
-            return setError('Password do not match')
-        }
-        
-        try{
-            setError("")
-            setLoading(true)
-            // console.log(emailRef.current.value,passwordRef.current.value)
-            const res = await signup(emailRef.current.value,passwordRef.current.value)
-            try{
-                await setDoc(doc(db, "Users", res.user.uid), {
-                    email:emailRef.current.value,
-                    userName:emailRef.current.value,
-                    userType:"",
-                    profilePic:"",
-                    userId:userIdRef.current.value,
-                    timeStamp: serverTimestamp()
-                });
-
-                await setDoc(doc(db, "UsersChats", res.user.uid), {
-                });
-                navigate('/login')
-            }catch(r){console.log(r)}
-        }catch{
-            console.log(error)
-            setError('Failed to Create an account');
-            setLoading(false)
-        }
-
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+      return setError("Password do not match");
     }
 
-    
+    try {
+      setError("");
+      setLoading(true);
+      // console.log(emailRef.current.value,passwordRef.current.value)
+      const res = await signup(
+        emailRef.current.value,
+        passwordRef.current.value
+      );
+      try {
+        await setDoc(doc(db, "Users", res.user.uid), {
+          email: emailRef.current.value,
+          userName: emailRef.current.value,
+          userType: "",
+          profilePic: "",
+          userId: userIdRef.current.value,
+          timeStamp: serverTimestamp(),
+        });
+
+        await setDoc(doc(db, "UsersChats", res.user.uid), {});
+        navigate("/login");
+      } catch (r) {
+        console.log(r);
+      }
+    } catch {
+      console.log(error);
+      setError("Failed to Create an account");
+      setLoading(false);
+    }
+  }
 
   return (
-        <div className='container'>
-            <Header/>
-            <div className='error'>
-                {error && <span>{error}</span>}
-            </div>
-            <div className='form-signup'>
-                    <form onSubmit={handleSubmit}>
-                        <h1 className='signup_h'>SignUp</h1>
-                        <br/>
-                        <input type='email' placeholder='Email' ref={emailRef} required />
-                        <input type='password' placeholder='Password'ref={passwordRef} required />
-                        <input type='password' placeholder='Confirm Password' ref={confirmPasswordRef} required/>
-                        <input type='text' placeholder='User Id' ref={userIdRef} required/>
-                        <br/>
-                        <button dissabled = {loading}>Sign Up</button>
-                    </form>
-                    <div className='to_login'>
-                        Already Have An Account?
-                        <br/>
-                        <Link to="/login">LogIn</Link>
-                    </div>
-            </div>
+    <div className="container">
+      <Header />
+      <div className="error">{error && <span>{error}</span>}</div>
+      <div className="form-signup">
+        <form onSubmit={handleSubmit}>
+          <h1 className="signup_h">SignUp</h1>
+          <br />
+          <input type="email" placeholder="Email" ref={emailRef} required />
+          <input
+            type="password"
+            placeholder="Password"
+            ref={passwordRef}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            ref={confirmPasswordRef}
+            required
+          />
+          <input type="text" placeholder="User Id" ref={userIdRef} required />
+          <br />
+          <button dissabled={loading}>Sign Up</button>
+        </form>
+        <div className="to_login">
+          Already Have An Account?
+          <br />
+          <Link to="/login">LogIn</Link>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
