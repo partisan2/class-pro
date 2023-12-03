7e50cebd3df86d31b07e9970942c579ecad5f074
rinas21 <s92064902@ousl.lk>
2023-12-03 16:21:32 +0530

    events updated
    css updated and added

--- src/components/pages/Events.js
+++ src/components/pages/Events.js
@@ -1,132 +1,189 @@
-import React, { useEffect, useState } from 'react'
-import HeaderDashboard from '../HeaderDashboard'
-import { Link } from 'react-router-dom'
-import { db } from '../../firebase'
-import { getDocs,collection,deleteDoc,doc,getDoc,query,orderBy } from "firebase/firestore";
-import { useAuth } from '../../contexts/AuthContext'
-import FooterDashboard from '../FooterDashboard';
-
-
+import React, { useEffect, useState } from "react";
+import HeaderDashboard from "../HeaderDashboard";
+import { Link } from "react-router-dom";
+import { db } from "../../firebase";
+import {
+  getDocs,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import { useAuth } from "../../contexts/AuthContext";
+import FooterDashboard from "../FooterDashboard";
+import './events.css';
 
 function Events() {
-  const [ documents,setDocuments ] = useState([])
-  const {currentUser} = useAuth();
-  const [ userType, setUserType ] =useState()
-  
-  //fetch data from db
-  useEffect(()=>{
-    const fetchData = async () =>{
-      let list = []
-      try{
-        const querySnapshot = await getDocs(query(collection(db, "Events"),orderBy("eventDate")));
-        querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, " => ", doc.data());
-        list.push({ id : doc.id ,...doc.data()})
-      });
-      setDocuments(list)
-      // console.log(list)
+  const [documents, setDocuments] = useState([]);
+  const { currentUser } = useAuth();
+  const [userType, setUserType] = useState();
 
-      }catch(r){console.log(r)}
-    }
-    fetchData()
-  },[])
+  //fetch data from db
+  useEffect(() => {
+    const fetchData = async () => {
+      let list = [];
+      try {
+        const querySnapshot = await getDocs(
+          query(collection(db, "Events"), orderBy("eventDate"))
+        );
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          // console.log(doc.id, " => ", doc.data());
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setDocuments(list);
+        // console.log(list)
+      } catch (r) {
+        console.log(r);
+      }
+    };
+    fetchData();
+  }, []);
 
   // get usertype
-  useEffect(()=>{
-    const fetchData = async () =>{
-      try{
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
         const docRef = doc(db, "Users", currentUser.uid);
         const docSnap = await getDoc(docRef);
-        
+
         if (docSnap.exists()) {
           // console.log("Document data:", docSnap.data().email);
-          setUserType(docSnap.data().userType)
+          setUserType(docSnap.data().userType);
           // console.log(docSnap.data().profilePic)
         } else {
           // docSnap.data() will be undefined in this case
           console.log("No such document!");
         }
-      }catch(r){console.log(r)}
-    }
-    fetchData()
-  },[currentUser.uid])
+      } catch (r) {
+        console.log(r);
+      }
+    };
+    fetchData();
+  }, [currentUser.uid]);
 
-  const event = documents?.map(({eventName,eventDate,eventTime,eventVenue,eventDiscription,eventId},index)=>{
-    return <EventLayout
-      key={index}
-      eventName={eventName}
-      eventDate={eventDate}
-      eventTime={eventTime}
-      eventVenue={eventVenue}
-      eventDiscription={eventDiscription}
-      eventId={eventId}                               
-    />
-  })
+  const event = documents?.map(
+    (
+      {
+        eventName,
+        eventDate,
+        eventTime,
+        eventVenue,
+        eventDiscription,
+        eventId,
+      },
+      index
+    ) => {
+      return (
+        <EventLayout
+          key={index}
+          eventName={eventName}
+          eventDate={eventDate}
+          eventTime={eventTime}
+          eventVenue={eventVenue}
+          eventDiscription={eventDiscription}
+          eventId={eventId}
+        />
+      );
+    }
+  );
 
   return (
-    <div className='eve-wrapper'>
-      <HeaderDashboard/>
-        <br/>
-        {userType === "teacher" && <span  className='eve-link'><Link to='/add-events'>Add Events</Link></span>}
-        <div className='eve-block'>
-          {event}
-        </div>
-      <FooterDashboard/>
-    </div>
-  )
-}
-
-function EventLayout({eventName,eventDate,eventTime,eventVenue,eventDiscription,eventId}){
-  const {currentUser} = useAuth();
-  const [ userType, setUserType ] =useState()                                     
-
-  useEffect(()=>{
-    const fetchData = async () =>{
-      try{
-        const docRef = doc(db, "Users", currentUser.uid);
-        const docSnap = await getDoc(docRef);
-        
-        if (docSnap.exists()) {
-          // console.log("Document data:", docSnap.data().email);
-          setUserType(docSnap.data().userType)
-          // console.log(docSnap.data().profilePic)
-        } else {
-          // docSnap.data() will be undefined in this case
-          console.log("No such document!");
-        }
-      }catch(r){console.log(r)}
-    }
-    fetchData()
-  },[currentUser.uid])
-
-  async function deleteEvent(e){
-    e.preventDefault()
-    try{
-      await deleteDoc(doc(db, "Events",eventId))
-      alert("Event Removed")
-      window.location.reload()
-    }catch(r){}
-  }
-
-
-  return(
-    <div className='event-component'>
-      <h3>{eventName}</h3>
-      <br/>
-      <div>
-        <p>Event Date : {eventDate}</p>
-        <p>Event Time : {eventTime}</p>
-        <p>Event Venue : {eventVenue}</p>
-        <p>Event Discription : {eventDiscription}</p>
-      </div>
-      {userType === "teacher" && 
-      <div>
-        <br/>
-        <span><button onClick={deleteEvent}>Delete</button></span>
-      </div>}
+    <div className="eve-wrapper">
+      <HeaderDashboard />
+      <br />
+      {userType === "teacher" && (
+        <span className="eve-link">
+          <Link to="/add-events">Add Events</Link>
+        </span>
+      )}
+      <div className="eve-block">{event}</div>
+      <FooterDashboard />
     </div>
   );
 }
 
-export default Events
+function EventLayout({
+  eventName,
+  eventDate,
+  eventTime,
+  eventVenue,
+  eventDiscription,
+  eventId,
+}) {
+  const { currentUser } = useAuth();
+  const [userType, setUserType] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const docRef = doc(db, "Users", currentUser.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          // console.log("Document data:", docSnap.data().email);
+          setUserType(docSnap.data().userType);
+          // console.log(docSnap.data().profilePic)
+        } else {
+          // docSnap.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      } catch (r) {
+        console.log(r);
+      }
+    };
+    fetchData();
+  }, [currentUser.uid]);
+
+  async function deleteEvent(e) {
+    e.preventDefault();
+    try {
+      await deleteDoc(doc(db, "Events", eventId));
+      alert("Event Removed");
+      window.location.reload();
+    } catch (r) {}
+  }
+  // Function to generate a random color in hexadecimal format
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  return (
+    <div className="eve-main-content">
+      <div
+        className="eve-event-component"
+        style={{ backgroundColor: getRandomColor() }}
+      >
+        <h3 className="eve-event-name">{eventName}</h3>
+        <br />
+        <div>
+          <p className="eve-event-info">Event Date : {eventDate}</p>
+          <p className="eve-event-info">Event Time : {eventTime}</p>
+          <p className="eve-event-info">Event Venue : {eventVenue}</p>
+          <p className="eve-event-info">
+            Event Description : {eventDiscription}
+          </p>
+        </div>
+        {userType === "teacher" && (
+          <div>
+            <br />
+            <span>
+              <button className="eve-delete-button" onClick={deleteEvent}>
+                Delete
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Events;
