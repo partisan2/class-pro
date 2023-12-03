d5cd369e01311e1b5d653fe0717fe5fff67f35e5
Vihan Gayathra <s92068813@ousl.lk>
2023-12-03 18:13:04 +0530

    updated

--- src/components/Messenger.js
+++ src/components/Messenger.js
@@ -2,14 +2,20 @@ import React from 'react'
 import Sidebar from './chat/Sidebar'
 import Chat from './chat/Chat'
 import './Messenger.css'
+import HeaderDashboard from './HeaderDashboard'
+import FooterDashboard from './FooterDashboard'
 
 function Messenger() {
   return (
+    <div>
+      <HeaderDashboard/>
     <div className='messenger'>
       <div className='containerm'>
         <Sidebar/>
         <Chat/>
       </div>
+    </div>
+    <FooterDashboard/>
     </div>
   )
 }
